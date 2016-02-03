286d7bdef793a83b70dffbc92b5198f060215f91
Jeep <jlevac@pharmacists.ca>
2016-02-03 10:56:51 -0500

    First commit

---
+++ listit.py
@@ -0,0 +1,8 @@
+import requests
+import json
+
+r = requests.get(r'http://reddit.com/r/climbing/new/.json')
+data = r.json();
+
+for child in data['data']['children']:
+    print "\t", "[", child['data']['id'] , "]", child['data']['title']
