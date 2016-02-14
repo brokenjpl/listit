6be66f32f023cf9864b59a6548e9e152782c8adf
Jeep <levacjeep@gmail.com>
2016-02-14 16:11:54 -0500

    converted script to use classes

--- actions/actions.py
+++ actions/actions.py
@@ -1,12 +1,21 @@
 from tabulate import tabulate
+import os.path
+import json
+from tabulate import tabulate
+import webbrowser
+from colorama import init, Fore, Style
+import requests.auth
+import requests
+import sys
 import configparser
 
 '''
-    Base class for actions
+    Base class for actions, handles the view of the front page
 '''
 class ListitAction(object):
 
     def __init__(self):
+        init()
         ##########################
         # Configs
         ##########################
@@ -21,30 +30,35 @@ class ListitAction(object):
         headers = {"User-Agent": "ListItClient/0.1 by levacjeep"}
         response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
         jsonResponse = response.json()
-        access_token = jsonResponse['access_token']
 
-        #############
-        # Action
-        ############
-        headers = {"Authorization": "bearer " + access_token, "User-Agent": "ListItClient/0.1 by levacjeep"}
+        #set the access_token for later use
+        self.access_token = jsonResponse['access_token']
 
     def build_uri(self):
         return "https://oauth.reddit.com"
 
     def fetch_response(self):
-        uri = build_uri()
+        uri = self.build_uri()
+        headers = {"Authorization": "bearer " + self.access_token, "User-Agent": "ListItClient/0.1 by levacjeep"}
         response =  requests.get(uri, headers=headers)
         return response.json()
 
-    def print(self, data):
+    def output(self, data):
         list=[]
         for child in data['data']['children']:
-            list.append(["[" + child['data']['name'] + "]", "["+ child['data']['domain']+"]", trimit(child['data']['title'], 150)])
+            list.append(["[" + child['data']['name'] + "]", "["+ child['data']['domain']+"]", self.trimit(child['data']['title'], 150)])
         print(tabulate(list))
 
     def execute(self):
-        response = fetch_response()
-        print(response)
+        response = self.fetch_response()
+        self.output(response)
+
+    #################
+    ## Helpers
+    ################
+    def trimit(self, s, l):
+        return s if len(s) <= l else s[0:l-3] + '...'
+
 
 '''
     Handles the creation of a Comments action. Comments typically required the ID of the post (view_id)
@@ -52,7 +66,20 @@ class ListitAction(object):
 class CommentsAction(ListitAction):
 
     def __init__(self, view_id):
-        this.view_id = view_id
+        super().__init__()
+        self.view_id = view_id
+
+    def build_uri(self):
+        return super().build_uri() + "/comments/" + self.view_id[3:]
+
+    def output(self, data):
+        list=[]
+        print("-----------------------------------------------------------------------------------------------")
+        for child in data[1]['data']['children'][:3]:
+            print(Fore.YELLOW + "[" + child['data']['name'] + "] [score: " + str(child['data']['score']) + "] [replies: " + str(len(child['data']['replies'])) +"]" + Style.RESET_ALL)
+            print(child['data']['body'])
+            print("-----------------------------------------------------------------------------------------------")
+        print("")
 
 '''
     Handles the viewing of a post
@@ -60,14 +87,14 @@ class CommentsAction(ListitAction):
 class ViewAction(ListitAction):
 
     def __init__(self, view_id):
-        super.__init__()
+        super().__init__()
         self.view_id = view_id
 
     def build_uri(self):
-        return super(ViewAction, self, view_id).build_url() + "/by_id/" + this.view 
+        return super().build_uri() + "/by_id/" + self.view_id 
 
-    def print(self):
-        data = super.fetch_response(self);
+    def output(self, data):
+        data = super().fetch_response();
         print("------------------------------------------------------------------------")
         print(data['data']['children'][0]['data']['title'])
         print("------------------------------------------------------------------------")
@@ -79,5 +106,24 @@ class ViewAction(ListitAction):
 class BrowserAction(ListitAction):
 
     def __init__(self, view_id):
-        super.__init__(self)
-	this.view_id = view_id
+        super().__init__()
+        self.view_id = view_id
+
+    def build_uri(self):
+       return super().build_uri() + "/by_id/" + view_id
+
+    def execute(self):
+        data = self.fetch_response()
+        webbrowser.open_new_tab('http://reddit.com' + data['data']['children'][0]['data']['permalink'])
+
+'''
+    Handles viewing a specific subreddit
+'''
+class SubredditAction(ListitAction):
+
+    def __init__(self, subreddit):
+        super().__init__()
+        self.subreddit = subreddit
+
+    def build_uri(self):
+        return super().build_uri() + "/r/" + self.subreddit
--- listit.py
+++ listit.py
@@ -1,93 +1,30 @@
-import sys
 import configparser
-import requests
-import requests.auth
-import json
 import argparse
-import webbrowser
-import os.path
-from tabulate import tabulate
-from colorama import init, Fore, Style
-import actions
 
-init()
-
-#################
-## Helpers
-################
-def trimit(s, l):
-    return s if len(s) <= l else s[0:l-3] + '...'
-
-##########################
-# Configs
-##########################
-config = configparser.RawConfigParser()
-config.read(os.path.expanduser('~') + '/.listit/listit.cfg')
+from actions.actions import *
 
 ##########################
 #Args
 #########################
 parser = argparse.ArgumentParser(description='CLI Reddit.')
 parser.add_argument('-sr', '-subreddit', required=False, default=False, help="Enter the subreddit (no /r)")
-parser.add_argument('-view', required=False, default=False, help="Pass the id of the post you want to view")
-parser.add_argument('-browser', required=False, default=False, help="Pass the id of the post you want to view in your browser")
-parser.add_argument('-comments', required=False, default=False, help="Pass the id of the post to view the comments")
+parser.add_argument('-view', '-v', required=False, default=False, help="Pass the id of the post you want to view")
+parser.add_argument('-browser', '-b', required=False, default=False, help="Pass the id of the post you want to view in your browser")
+parser.add_argument('-comments', '-c', required=False, default=False, help="Pass the id of the post to view the comments")
 parser.add_argument('-ct', '-comment_tree', required=False, default=False, help="Pass the id of the comment to view the comment tree")
 args = parser.parse_args()
 
-############################
-## OAuth setup
-###########################
-client_auth = requests.auth.HTTPBasicAuth(config.get('OAuth', 'key.private'), config.get('OAuth', 'key.public'))
-post_data = {"grant_type": "password", "username": config.get('OAuth', 'reddit.username'), "password": config.get('OAuth', 'reddit.password')}
-headers = {"User-Agent": "ListItClient/0.1 by levacjeep"}
-response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
-jsonResponse = response.json()
-access_token = jsonResponse['access_token']
+action = None
 
-#############
-# Action
-############
-headers = {"Authorization": "bearer " + access_token, "User-Agent": "ListItClient/0.1 by levacjeep"}
-
-uri = "https://oauth.reddit.com"
-if args.sr :
-    uri += "/r/" + args.sr
-if args.view or args.browser :
-    uri += "/by_id/" + (args.view if args.view else args.browser)
-if args.comments :
-    uri += "/comments/"+args.comments[3:]
-
-response = requests.get(uri, headers=headers)
-
-data = response.json()
-
-##Debug
-f = open('ouput.txt', 'w')
-f.write(response.text)
-f.close()
-###################
-# Parse and output
-###################
-if args.browser:
-    webbrowser.open_new_tab('http://reddit.com' + data['data']['children'][0]['data']['permalink'])
-    sys.exit()
+if args.sr:
+    action = SubredditAction(args.sr)
 if args.view:
-    print("------------------------------------------------------------------------")
-    print(data['data']['children'][0]['data']['title'])
-    print("------------------------------------------------------------------------")
-    print(data['data']['children'][0]['data']['selftext'])
-if args.comments:
-    response = requests.get("https://oauth.reddit.com/comments/" + args.comments[3:], headers=headers)
-    list=[]
-    print("-----------------------------------------------------------------------------------------------")
-    for child in data[1]['data']['children'][:3]:
-        print(Fore.YELLOW + "[" + child['data']['name'] + "] [score: " + str(child['data']['score']) + "] [replies: " + str(len(child['data']['replies'])) +"]" + Style.RESET_ALL)
-        print(child['data']['body'])
-        print("-----------------------------------------------------------------------------------------------")
-    print("")
-if args.sr or (not args.view and not args.comments):
-    list=[]
-    for child in data['data']['children']:
-        list.append(["[" + child['data']['name'] + "]", "["+ child['data']['domain']+"]", trimit(child['data']['title'], 150)])
-    print(tabulate(list))
+    action = ViewAction(args.view)
+if args.browser:
+    action = BrowserAction(args.browser)
+if args.comments: 
+    action = CommentsAction(args.comments)
+if not args.sr and not args.view and not args.browser and not args.comments:
+    action = ListitAction()
+
+action.execute()
