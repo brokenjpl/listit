1d1067f3f9f29095de35abbdf6fab2841e5f8553
Jeep <jlevac@pharmacists.ca>
2016-02-23 10:59:08 -0500

    cleanup

--- listit.py
+++ listit.py
@@ -4,14 +4,19 @@ import argparse
 from actions.actions import *
 
 ##########################
+# Functions
+##########################
+
+##########################
 #Args
-#########################
-parser = argparse.ArgumentParser(description='CLI Reddit.')
-parser.add_argument('-sr', '-subreddit', required=False, default=False, help="Enter the subreddit (no /r)")
-parser.add_argument('-view', '-v', required=False, default=False, help="Pass the id of the post you want to view")
-parser.add_argument('-browser', '-b', required=False, default=False, help="Pass the id of the post you want to view in your browser")
-parser.add_argument('-comments', '-c', required=False, default=False, help="Pass the id of the post to view the comments")
-parser.add_argument('-ct', '-comment_tree', required=False, default=False, help="Pass the id of the comment to view the comment tree")
+##########################
+parser = argparse.ArgumentParser(description='CLI Reddit: Listit')
+parser.add_argument('-sr', '-subreddit',  help="Enter the subreddit (no /r)")
+parser.add_argument('-view', '-v',  help="Pass the id of the post you want to view")
+parser.add_argument('-browser', '-b',  help="Pass the id of the post you want to view in your browser")
+parser.add_argument('-comments', '-c',  help="Pass the id of the post to view the comments")
+parser.add_argument('-ct', '-comment_tree',  help="Pass the id of the comment to view the comment tree")
+
 args = parser.parse_args()
 
 action = None
@@ -24,7 +29,7 @@ if args.browser:
     action = BrowserAction(args.browser)
 if args.comments: 
     action = CommentsAction(args.comments)
-if not args.sr and not args.view and not args.browser and not args.comments:
+if action == None:
     action = ListitAction()
 
 action.execute()
